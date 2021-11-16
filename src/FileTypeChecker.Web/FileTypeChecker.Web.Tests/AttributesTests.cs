b7718d79e9255b6f93208bdac90d2cd7bcc49692
Aleksandar J. Mitev <hello@aleksandar.it>
2021-11-16 13:13:29 +0200

    Rewrite unittests with xUnit

--- src/FileTypeChecker.Web/FileTypeChecker.Web.Tests/AttributesTests.cs
+++ src/FileTypeChecker.Web/FileTypeChecker.Web.Tests/AttributesTests.cs
@@ -1,248 +1,248 @@
-﻿namespace FileTypeChecker.Web.Tests
+﻿namespace FileTypeChecker.Web.Theorys
 {
     using FileTypeChecker.Types;
     using FileTypeChecker.Web.Attributes;
-    using NUnit.Framework;
+    using FileTypeChecker.Web.Tests;
     using System;
+    using Xunit;
 
-    [TestFixture]
-    public class AttributesTests
+    public class AttribuTheoryests
     {
 
-        [Test]
-        [TestCase("365-doc.docx", true)]
-        [TestCase("test.pdf", true)]
-        [TestCase("test.doc", true)]
-        [TestCase("test.png", false)]
-        [TestCase("test.zip", false)]
+        [Theory]
+        [InlineData("365-doc.docx", true)]
+        [InlineData("test.pdf", true)]
+        [InlineData("test.doc", true)]
+        [InlineData("test.png", false)]
+        [InlineData("test.zip", false)]
         public void AllowDocumentsAttribute_ShouldAllowOnlyDocuments(string fileName, bool expectedResult)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new AllowDocumentsAttribute();
+            var attributeToTheory = new AllowDocumentsAttribute();
 
-            Assert.AreEqual(expectedResult, attributeToTest.IsValid(stream));
+            Assert.Equal(expectedResult, attributeToTheory.IsValid(stream));
         }
 
-        [Test]
-        [TestCase(new[] { "365-doc.docx", "test.pdf", "test.doc" }, true)]
-        [TestCase(new[] { "365-doc.docx", "test.doc" }, true)]
-        [TestCase(new[] { "365-doc.docx", "test.zip" }, false)]
+        [Theory]
+        [InlineData(new[] { "365-doc.docx", "test.pdf", "test.doc" }, true)]
+        [InlineData(new[] { "365-doc.docx", "test.doc" }, true)]
+        [InlineData(new[] { "365-doc.docx", "test.zip" }, false)]
         public void AllowDocumentsAttribute_ShouldAllowOnlyDocumentsIfTheInputIsCollection(string[] files, bool expectedResult)
         {
             var stream = FileHelpers.ReadFiles(files);
 
-            var attributeToTest = new AllowDocumentsAttribute();
+            var attributeToTheory = new AllowDocumentsAttribute();
 
-            Assert.AreEqual(expectedResult, attributeToTest.IsValid(stream));
+            Assert.Equal(expectedResult, attributeToTheory.IsValid(stream));
         }
 
-        [Test]
-        [TestCase("test.bmp", new[] { Bitmap.TypeExtension }, true)]
-        [TestCase("test.bmp", new[] { JointPhotographicExpertsGroup.TypeExtension }, false)]
-        [TestCase("test.jpg", new[] { JointPhotographicExpertsGroup.TypeExtension }, true)]
-        [TestCase("test.jpg", new[] { Bitmap.TypeExtension, JointPhotographicExpertsGroup.TypeExtension, GraphicsInterchangeFormat89.TypeExtension }, true)]
-        [TestCase("test.jpg", new[] { RarArchive.TypeExtension }, false)]
-        [TestCase("test.zip", new[] { ZipFile.TypeExtension }, true)]
+        [Theory]
+        [InlineData("test.bmp", new[] { Bitmap.TypeExtension }, true)]
+        [InlineData("test.bmp", new[] { JointPhotographicExpertsGroup.TypeExtension }, false)]
+        [InlineData("test.jpg", new[] { JointPhotographicExpertsGroup.TypeExtension }, true)]
+        [InlineData("test.jpg", new[] { Bitmap.TypeExtension, JointPhotographicExpertsGroup.TypeExtension, GraphicsInterchangeFormat89.TypeExtension }, true)]
+        [InlineData("test.jpg", new[] { RarArchive.TypeExtension }, false)]
+        [InlineData("test.zip", new[] { ZipFile.TypeExtension }, true)]
         public void AllowedTypeAttribute_ShouldAllowOnlyPointedTypes(string fileName, string[] allowedExtensions, bool expectedResult)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new AllowedTypesAttribute(allowedExtensions);
+            var attributeToTheory = new AllowedTypesAttribute(allowedExtensions);
 
-            Assert.AreEqual(expectedResult, attributeToTest.IsValid(stream));
+            Assert.Equal(expectedResult, attributeToTheory.IsValid(stream));
         }
 
-        [Test]
-        [TestCase("test.zip", null)]
-        [TestCase("test.zip", new string[] { })]
+        [Theory]
+        [InlineData("test.zip", null)]
+        [InlineData("test.zip", new string[] { })]
         public void AllowTypesAttribute_ShouldThrowExceptionIfNoExtensionsProvided(string fileName, string[] allowedExtensions)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new AllowedTypesAttribute(allowedExtensions);
+            var attributeToTheory = new AllowedTypesAttribute(allowedExtensions);
 
-            Assert.Throws<InvalidOperationException>(() => attributeToTest.IsValid(stream));
+            Assert.Throws<InvalidOperationException>(() => attributeToTheory.IsValid(stream));
         }
 
-        [Test]
-        [TestCase("test.zip", new[] { ZipFile.TypeExtension }, false)]
-        [TestCase("test.png", new[] { ZipFile.TypeExtension }, true)]
-        [TestCase("test.png", new[] { ZipFile.TypeExtension, ExtensibleArchive.TypeExtension, JointPhotographicExpertsGroup.TypeExtension }, true)]
-        [TestCase("test.png", new[] { ZipFile.TypeExtension, PortableNetworkGraphic.TypeExtension, JointPhotographicExpertsGroup.TypeExtension }, false)]
+        [Theory]
+        [InlineData("test.zip", new[] { ZipFile.TypeExtension }, false)]
+        [InlineData("test.png", new[] { ZipFile.TypeExtension }, true)]
+        [InlineData("test.png", new[] { ZipFile.TypeExtension, ExtensibleArchive.TypeExtension, JointPhotographicExpertsGroup.TypeExtension }, true)]
+        [InlineData("test.png", new[] { ZipFile.TypeExtension, PortableNetworkGraphic.TypeExtension, JointPhotographicExpertsGroup.TypeExtension }, false)]
         public void ForbidTypesAttribute_ShouldForbidPointedTypes(string fileName, string[] forbidedExtensions, bool expectedResult)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new ForbidTypesAttribute(forbidedExtensions);
+            var attributeToTheory = new ForbidTypesAttribute(forbidedExtensions);
 
-            Assert.AreEqual(expectedResult, attributeToTest.IsValid(stream));
+            Assert.Equal(expectedResult, attributeToTheory.IsValid(stream));
         }
 
-        [Test]
-        [TestCase("test.zip", null)]
-        [TestCase("test.zip", new string[] { })]
+        [Theory]
+        [InlineData("test.zip", null)]
+        [InlineData("test.zip", new string[] { })]
         public void ForbidTypesAttribute_ShouldThrowExceptionIfNoExtensionsProvided(string fileName, string[] allowedExtensions)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new ForbidTypesAttribute(allowedExtensions);
+            var attributeToTheory = new ForbidTypesAttribute(allowedExtensions);
 
-            Assert.Throws<InvalidOperationException>(() => attributeToTest.IsValid(stream));
+            Assert.Throws<InvalidOperationException>(() => attributeToTheory.IsValid(stream));
         }
 
 
-        [Test]
-        [TestCase("test.png", false)]
-        [TestCase("test.jpg", false)]
-        [TestCase("test.bmp", false)]
-        [TestCase("test.zip", true)]
-        [TestCase("test.7z", true)]
-        [TestCase("test.bz2", true)]
-        [TestCase("test.gz", true)]
-        [TestCase("365-doc.docx", false)]
+        [Theory]
+        [InlineData("test.png", false)]
+        [InlineData("test.jpg", false)]
+        [InlineData("test.bmp", false)]
+        [InlineData("test.zip", true)]
+        [InlineData("test.7z", true)]
+        [InlineData("test.bz2", true)]
+        [InlineData("test.gz", true)]
+        [InlineData("365-doc.docx", false)]
         public void OnlyArchiveAttribute_ShouldReturnValidateIfTheFileIsArchive(string fileName, bool expectedResult)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new AllowArchivesAttribute();
+            var attributeToTheory = new AllowArchivesAttribute();
 
-            Assert.AreEqual(expectedResult, attributeToTest.IsValid(stream));
+            Assert.Equal(expectedResult, attributeToTheory.IsValid(stream));
         }
 
-        [Test]
-        [TestCase("test.png", true)]
-        [TestCase("test.jpg", true)]
-        [TestCase("test.bmp", true)]
-        [TestCase("test.zip", false)]
-        [TestCase("365-doc.docx", false)]
+        [Theory]
+        [InlineData("test.png", true)]
+        [InlineData("test.jpg", true)]
+        [InlineData("test.bmp", true)]
+        [InlineData("test.zip", false)]
+        [InlineData("365-doc.docx", false)]
         public void OnlyImageAttribute_ShouldValidateIfTheFileIsImage(string fileName, bool expectedResult)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new AllowImagesAttribute();
+            var attributeToTheory = new AllowImagesAttribute();
 
-            Assert.AreEqual(expectedResult, attributeToTest.IsValid(stream));
+            Assert.Equal(expectedResult, attributeToTheory.IsValid(stream));
         }
 
-        [Test]
-        [TestCase("test.exe")]
+        [Theory]
+        [InlineData("test.exe")]
         public void ForbidExecutableFilesAttribute_ShouldReturnInvalidIfTheFileIsExecutable(string fileName)
         {
             var stream = FileHelpers.ReadFile(fileName);
 
-            var attributeToTest = new ForbidExecutablesAttribute();
+            var attributeToTheory = new ForbidExecutablesAttribute();
 
-            Assert.IsFalse(attributeToTest.IsValid(stream));
+            Assert.False(attributeToTheory.IsValid(stream));
         }
 
-        [Test]
+        [Fact]
         public void OnlyImageAttribute_ShouldValidateMultupleFilesIfTheyAreImages()
         {
             var fileNames = new[] { "test.png", "test.jpg", "test.bmp" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attrebuteToTest = new AllowImagesAttribute();
+            var attrebuteToTheory = new AllowImagesAttribute();
 
-            Assert.IsTrue(attrebuteToTest.IsValid(files));
+            Assert.True(attrebuteToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void OnlyImageAttribute_ShouldReturnReturnFalseIfOneOfFilesIsNotImage()
         {
             var fileNames = new[] { "test.png", "test.jpg", "test.bmp", "test.exe" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attrebuteToTest = new AllowImagesAttribute();
+            var attrebuteToTheory = new AllowImagesAttribute();
 
-            Assert.IsFalse(attrebuteToTest.IsValid(files));
+            Assert.False(attrebuteToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void AllowArchiveOnlyAttribute_ShouldCanValidateMultipleFiles()
         {
             var fileNames = new[] { "test.zip", "test.7z", "test.bz2", "test.gz" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new AllowArchivesAttribute();
-            Assert.IsTrue(attributeToTest.IsValid(files));
+            var attributeToTheory = new AllowArchivesAttribute();
+            Assert.True(attributeToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void AllowArchiveOnlyAttribute_ShouldReturnTrueIfInputIsNull()
         {
-            var attributeToTest = new AllowArchivesAttribute();
-            Assert.IsTrue(attributeToTest.IsValid(null));
+            var attributeToTheory = new AllowArchivesAttribute();
+            Assert.True(attributeToTheory.IsValid(null));
         }
 
-        [Test]
+        [Fact]
         public void AllowArchiveOnlyAttribute_ShouldReturnFalseIfOneOfTheFilesIsNotArchive()
         {
             var fileNames = new[] { "test.zip", "test.7z", "test.jpg", "test.gz" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new AllowArchivesAttribute();
-            Assert.IsFalse(attributeToTest.IsValid(files));
+            var attributeToTheory = new AllowArchivesAttribute();
+            Assert.False(attributeToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void AllowTypesAttribute_ShouldValidateMultipleFiles()
         {
             var fileNames = new[] { "test.zip", "test.7z", "test.gz" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new AllowedTypesAttribute(ZipFile.TypeExtension, SevenZipFile.TypeExtension, Gzip.TypeExtension);
-            Assert.IsTrue(attributeToTest.IsValid(files));
+            var attributeToTheory = new AllowedTypesAttribute(ZipFile.TypeExtension, SevenZipFile.TypeExtension, Gzip.TypeExtension);
+            Assert.True(attributeToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void AllowTypesAttribute_ShouldReturnFalseIfOneOfTheFilesIsNotAllowed()
         {
             var fileNames = new[] { "test.zip", "test.7z", "test.gz" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new AllowedTypesAttribute(ZipFile.TypeExtension, SevenZipFile.TypeExtension);
-            Assert.IsFalse(attributeToTest.IsValid(files));
+            var attributeToTheory = new AllowedTypesAttribute(ZipFile.TypeExtension, SevenZipFile.TypeExtension);
+            Assert.False(attributeToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void ForbidExecutableAttribute_ShouldValidateMultipleFiles()
         {
             var fileNames = new[] { "test.zip", "test.7z", "test.jpg", "test.gz" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new ForbidExecutablesAttribute();
-            Assert.IsTrue(attributeToTest.IsValid(files));
+            var attributeToTheory = new ForbidExecutablesAttribute();
+            Assert.True(attributeToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void ForbidExecutableAttribute_ShouldReturnFalseIfOneOfFilesIsExecutable()
         {
             var fileNames = new[] { "test.zip", "test.7z", "test.exe", "test.gz" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new ForbidExecutablesAttribute();
-            Assert.IsFalse(attributeToTest.IsValid(files));
+            var attributeToTheory = new ForbidExecutablesAttribute();
+            Assert.False(attributeToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void ForbidTypesAttribute_ShouldValidateMultipleFiles()
         {
             var fileNames = new[] { "test.zip", "test.7z", "test.gz" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new ForbidTypesAttribute(JointPhotographicExpertsGroup.TypeExtension);
-            Assert.IsTrue(attributeToTest.IsValid(files));
+            var attributeToTheory = new ForbidTypesAttribute(JointPhotographicExpertsGroup.TypeExtension);
+            Assert.True(attributeToTheory.IsValid(files));
         }
 
-        [Test]
+        [Fact]
         public void ForbidTypesAttribute_ShouldReturnFalseIfOneOfFilesIsForbiden()
         {
             var fileNames = new[] { "test.zip", "test.jpg", "test.png" };
             var files = FileHelpers.ReadFiles(fileNames);
 
-            var attributeToTest = new ForbidTypesAttribute(ZipFile.TypeExtension);
-            Assert.IsFalse(attributeToTest.IsValid(files));
+            var attributeToTheory = new ForbidTypesAttribute(ZipFile.TypeExtension);
+            Assert.False(attributeToTheory.IsValid(files));
         }
     }
 }
--- src/FileTypeChecker.Web/FileTypeChecker.Web.Tests/IFormFileExtensionsTests.cs
+++ src/FileTypeChecker.Web/FileTypeChecker.Web.Tests/IFormFileExtensionsTests.cs
@@ -1,89 +1,88 @@
 ﻿namespace FileTypeChecker.Web.Tests
 {
     using FileTypeChecker.Types;
-    using NUnit.Framework;
+    using Xunit;
 
-    [TestFixture]
     public class IFormFileExtensionsTests
     {
-        [Test]
+        [Fact]
         public void Is_ShouldReturnTrueIfTheTypesMatch()
         {
             var formFile = FileHelpers.ReadFile("test.bmp");
             var expected = true;
             var actual = formFile.Is<Bitmap>();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
-        [Test]
+        [Fact]
         public void Is_ShouldReturnFalseIfTypesDidNotMatch()
         {
             var formFile = FileHelpers.ReadFile("test.bmp");
             var expected = false;
             var actual = formFile.Is<Gzip>();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
-        [Test]
+        [Fact]
         public void IsImage_ShouldReturnTrueIfTheTypesMatch()
         {
             var formFile = FileHelpers.ReadFile("test.jpg");
             var expected = true;
             var actual = formFile.IsImage();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
-        [Test]
+        [Fact]
         public void IsImage_ShouldReturnFalseIfTypesDidNotMatch()
         {
             var formFile = FileHelpers.ReadFile("test.exe");
             var expected = false;
             var actual = formFile.IsImage();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
-        [Test]
+        [Fact]
         public void IsArchive_ShouldReturnTrueIfTheTypesMatch()
         {
             var formFile = FileHelpers.ReadFile("test.zip");
             var expected = true;
             var actual = formFile.IsArchive();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
-        [Test]
+        [Fact]
         public void IsArchive_ShouldReturnFalseIfTypesDidNotMatch()
         {
             var formFile = FileHelpers.ReadFile("test.bmp");
             var expected = false;
             var actual = formFile.IsArchive();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
-        [Test]
+        [Fact]
         public void IsExecutable_ShouldReturnTrueIfTheTypesMatch()
         {
             var formFile = FileHelpers.ReadFile("test.exe");
             var expected = true;
             var actual = formFile.IsExecutable();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
-        [Test]
+        [Fact]
         public void IsExecutable_ShouldReturnFalseIfTypesDidNotMatch()
         {
             var formFile = FileHelpers.ReadFile("test.bmp");
             var expected = false;
             var actual = formFile.IsExecutable();
 
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
     }
 }
