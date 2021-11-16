91bdff4e18fe0ad09be931bdc98b90ac15607383
Aleksandar J. Mitev <hello@aleksandar.it>
2021-11-16 09:18:06 +0200

    Adding samples project and functionality to register custom types via IServiceCollection

---
+++ src/FileTypeChecker.Web/FileTypeChecker.Web/Infrastructure/IServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace FileTypeChecker.Web.Infrastructure
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Reflection;
+
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFileTypesValidation(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            FileTypeValidator.RegisterCustomTypes(assemblies);
+
+            return services;
+        }
+    }
+}
